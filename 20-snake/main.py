985aaa05c106ed60b7cdd0d7a2bb7fb444fd03ac
johnahnz0rs <johnahn@z0rs.org>
2022-08-09 02:43:14 -0700

    cont snake

--- 20-snake/main.py
+++ 20-snake/main.py
@@ -1,13 +1,14 @@
 from turtle import Turtle, Screen
+import time
 t = Turtle()
 screen = Screen()
-starting_positions = [(0,0), (-20,0), (-40,0)]
-segments = []
-game_is_on = True
-
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake")
+screen.tracer(0)
+starting_positions = [(0,0), (-20,0), (-40,0)]
+segments = []
+game_is_on = True
 
 
 for p in starting_positions:
@@ -17,9 +18,19 @@ for p in starting_positions:
     new_segment.goto(p)
     segments.append(new_segment)
 
+screen.update()
+# while game_is_on:
+for _ in range(0,10):
+    
+    for n in range(len(segments)-1, 0, -1):
+        new_x = segments[n-1].xcor()
+        new_y = segments[n-1].ycor()
+        segments[n].goto(new_x, new_y)
+    segments[0].forward(20)
+    segments[0].left(90)
 
-while game_is_on:
-    game_is_on = False
+    screen.update()
+    time.sleep(.5)
 
 
 
