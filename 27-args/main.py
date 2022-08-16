e7e7e03cfe90097afd471e91d5b55519a1414c11
johnahnz0rs <johnahn@z0rs.org>
2022-08-15 23:57:39 -0700

    start day 27

---
+++ 27-args/main.py
@@ -0,0 +1,16 @@
+import tkinter
+
+
+window = tkinter.Tk()
+window.title("LOL")
+window.minsize(width=500, height=300)
+
+
+my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+
+
+
+window.mainloop()
