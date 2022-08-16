db817017284042738d55e71a65b2c6ead67c2f63
johnahnz0rs <johnahn@z0rs.org>
2022-08-15 23:39:36 -0700

    finished day 26

--- 26-nato-alphabet/main.py
+++ 26-nato-alphabet/main.py
@@ -2,20 +2,11 @@ import pandas
 nato = pandas.read_csv("nato.csv") #nato is a DataFrame object now
 
 
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-nato_dict = {}
-for (_, s) in nato.iterrows():
-    l = s["letter"]
-    c = s["code"]
-    nato_dict[l] = c
-# print(nato_dict)
+#TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
+nato_dict = {row.letter:row.code for (_, row) in nato.iterrows()}
 
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 u_input = input("enter a short word (letters only plz): ").upper()
-# # new_var = [new_item for x in thing if]
-# coded_input = [nato_dict[u] for u in u_input]
-# new_var = [new_item for x in thing if]
 coded_input = [nato_dict[u] for u in u_input if u in nato_dict]
 print(coded_input)
