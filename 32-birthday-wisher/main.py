53cc9c704b89a9af159097539d4fe53859ccf950
johnahnz0rs <johnahn@z0rs.org>
2022-08-25 02:02:21 -0700

    started day 32

---
+++ 32-birthday-wisher/main.py
@@ -0,0 +1,10 @@
+import smtplib
+my_email = "johnahnz0rsisl33t@yahoo.com"
+my_password = "l33th4x0rsOMGwtf!!"
+
+
+connection = smtplib.SMTP("smtp.mail.yahoo.com")
+connection.starttls()
+connection.login(user=my_email, password=my_password)
+connection.sednmail(from_addr=my_email, to_addrs="johnahn123@gmail.com", msg="helloworld")
+connection.close()
