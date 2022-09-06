befa4982e6a5804944896244afb2aa4fdc33ef89
johnahnz0rs <johnahn@z0rs.org>
2022-09-05 22:55:33 -0700

    cont'd day 37

--- 37-habit-tracker/main.py
+++ 37-habit-tracker/main.py
@@ -1,1 +1,25 @@
+import os
+from dotenv import load_dotenv
 import requests
+
+load_dotenv()
+USERNAME = os.getenv("USERNAME")
+TOKEN = os.getenv("TOKEN")
+GRAPH_ID = os.getenv("GRAPH_ID")
+
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+graph_params = {
+    "date": "20220904",
+    "quantity": "180",
+}
+endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_ID}"
+
+new_graph = requests.post(url=endpoint, json=graph_params, headers=headers)
+print(new_graph.text)
+
+
+
+
