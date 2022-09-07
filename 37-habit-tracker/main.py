fdfcffcceb2925125d450642e46b97fd1d6b7143
johnahnz0rs <johnahn@z0rs.org>
2022-09-06 22:50:23 -0700

    finished day 37

--- 37-habit-tracker/main.py
+++ 37-habit-tracker/main.py
@@ -1,5 +1,9 @@
+
+# https://pixe.la/v1/users/johnahnz0rs/graphs/python-dr-yu.html
+
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 import requests
 
 load_dotenv()
@@ -7,19 +11,27 @@ USERNAME = os.getenv("USERNAME")
 TOKEN = os.getenv("TOKEN")
 GRAPH_ID = os.getenv("GRAPH_ID")
 
-
 headers = {
     "X-USER-TOKEN": TOKEN
 }
+today_date = datetime.now()
+today = today_date.strftime("%Y%m%d")
 graph_params = {
-    "date": "20220904",
-    "quantity": "180",
+    "date": today,
+    "quantity": input("how many minutes did you study python today?"),
 }
-endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_ID}"
 
-new_graph = requests.post(url=endpoint, json=graph_params, headers=headers)
-print(new_graph.text)
+new_entry_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_ID}"
+new_entry = requests.post(url=new_entry_endpoint, json=graph_params, headers=headers)
+print(new_entry.text)
 
+# update_entry_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_ID}/{today}"
+# update_entry = requests.put(url=update_entry_endpoint, json=graph_params, headers=headers)
+# print(update_entry.text)
+
+# delete_entry_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_ID}/{today}"
+# delete_entry = requests.delete(url=delete_entry_endpoint, headers=headers)
+# print(delete_entry.text)
 
 
 
