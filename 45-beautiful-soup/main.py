8323b55e09e89cd14965a21a30fc9a9f378f4441
johnahnz0rs <johnahn@z0rs.org>
2022-09-23 01:05:57 -0700

    finished day 45

--- 45-beautiful-soup/main.py
+++ 45-beautiful-soup/main.py
@@ -1,53 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-# with open("website.html") as file:
-
-#     content = file.read()
-
-# soup = BeautifulSoup(content, "html.parser")
-
-# print(soup)
-
-
-response = requests.get("https://news.ycombinator.com/news")
-ycombinator = BeautifulSoup(response.text, "html.parser")
-
-
-# first_article = ycombinator.select_one("a.titlelink")
-# first_article_title = first_article.get_text()
-# first_article_link = first_article["href"]
-
-# first_article_meta = ycombinator.select_one(".subtext .score ").get_text()
-# first_article_upvotes = first_article_meta.split(" ")[0]
-
-
-# print(f"title: {first_article_title}\nlink: {first_article_link}\nupvotes: {first_article_upvotes}")
-
-articles = ycombinator.select(".titlelink")
-# print(articles)
-titles = []
-links = []
-for a in articles:
-    titles.append(a.get_text())
-    links.append(a["href"])
-
-list_of_upvotes = ycombinator.select(".subtext .score")
-upvotes = [int(u.get_text().split()[0]) for u in list_of_upvotes]
-max_score = max(upvotes)
-max_score_index = upvotes.index(max_score)
-
-highest_voted_article = {
-    'title': titles[max_score_index],
-    'link': links[max_score_index],
-    'upvote': upvotes[max_score_index]
-}
-
-print(highest_voted_article)
 
 
 
-# print(titles)
-# print(links)
-# print(upvotes)
+# response = requests.get("https://news.ycombinator.com/news")
+# ycombinator = BeautifulSoup(response.text, "html.parser")
+
+
+
+response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+website_html = BeautifulSoup(response.text, "html.parser")
+
+
+titles_list = website_html.select("section .article-title-description h3.title")
+# print(titles) # this will be a list of strings - it was a list of h3 tags
+
+
+
+movie_titles = [title.getText() for title in titles_list]
+top_100_movies = movie_titles[::-1]
+# print(top_100_movies)
+
+with open("movies.txt", mode="w") as file:
+    for movie in top_100_movies:
+        file.write(f"{movie}\n")
+
+
 
