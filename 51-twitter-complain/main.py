63b2f469a1988a9865b226cee2e443f47459d1ba
johnahnz0rs <johnahn@z0rs.org>
2022-10-01 00:01:56 -0700

    finished day 51

--- 51-twitter-complain/main.py
+++ 51-twitter-complain/main.py
@@ -27,7 +27,7 @@ class InternetSpeedTwitterBot():
         self.up = None
 
     def tweeter_a_complaint(self, down, up):
-        print("tweeter_a_complaint()") # dev
+        print(f"tweeter_a_complaint(down={down}, up={up})") # dev
         self.driver.get(TWITTER_LOGIN_URL)
         # enter login username
         login_username = None
@@ -37,31 +37,44 @@ class InternetSpeedTwitterBot():
             except NoSuchElementException:
                 print("sleep - no login_username") # dev
                 time.sleep(1)
+        print("filling in username")
         login_username.send_keys(T_ID)
-        next_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]')
+        
+        # next_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]')
+        next_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]')
+        print("clicking next button")
         next_button.click()
         # enter pw
         login_pw = None
         while not login_pw:
             try:
-                login_pw = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+                # login_pw = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+                login_pw = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
             except NoSuchElementException:
                 print("sleep - no login_pw") # dev
                 time.sleep(1)
+        print("filling in pw")
         login_pw.send_keys(T_PW)
-        login_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div')
+        time.sleep(1)
+        # login_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div')
+        login_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div')
+        
+        print("clicking log in button")
         login_button.click()
         # write a tweet
         tweet_textarea = None
         while not tweet_textarea:
             try:
-                tweet_textarea = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div[2]/div/div/div/div/label/div[1]/div/div/div/div/div/div[2]/div/div/div/div')
+                # tweet_textarea = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div[2]/div/div/div/div/label/div[1]/div/div/div/div/div/div[2]/div/div/div/div')
+                tweet_textarea = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div[2]/div/div/div/div/label/div[1]/div/div/div/div/div/div[2]/div/div/div/div/span')
             except NoSuchElementException:
                 print("sleep - no tweet_texarea")
                 time.sleep(1)
         tweeter_msg = f"ew my internet speed is so slow. i'm only getting {down} down and {up} up. lagging like a mahfuqa"
+        print(f"prepping tweet - here's the msg:\n{tweeter_msg}")
         tweet_textarea.send_keys(tweeter_msg)
         tweet_button = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]')
+        print("clicking tweet button to send tweet")
         tweet_button.click()
 
 
@@ -78,6 +91,7 @@ class InternetSpeedTwitterBot():
             except NoSuchElementException:
                 print("sleep - start_speed_test_button") # dev
                 time.sleep(1)
+        print("clicking button to start the speed test")
         start_speed_test_button.click()
         # wait for test to finish then get results
         results_div = None
@@ -104,13 +118,15 @@ t_bot = InternetSpeedTwitterBot()
 
 # get my internet up/down speeds
 my_internet_speeds = t_bot.get_internet_speed()
+my_down_speed = my_internet_speeds["down_speed"]
+my_up_speed = my_internet_speeds["up_speed"]
 # print(f"speeds:\n{my_internet_speeds}")
 
 
 # if internet is slow, then tweeter at whomstever
-# if my_internet_speeds["down_speed"] < GUARANTEED_DOWN or my_internet_speeds["up_speed"] < GUARANTEED_UP:
-if my_internet_speeds["down_speed"] < 9999 or my_internet_speeds["up_speed"] < 100: # dev
-    t_bot.tweeter_a_complaint(down=my_internet_speeds["down_speed"], up=my_internet_speeds["up_speed"])
+# if my_down_speed < GUARANTEED_DOWN or my_up_speed < GUARANTEED_UP:
+if my_down_speed < 9999 or my_up_speed < 100: # dev
+    t_bot.tweeter_a_complaint(down=my_down_speed, up=my_up_speed)
 
 
 
