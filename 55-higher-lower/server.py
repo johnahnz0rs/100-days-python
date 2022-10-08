e6215b1eff1b1a2c7f7e33bf72f87088f4f80386
johnahnz0rs <johnahn@z0rs.org>
2022-10-08 15:02:26 -0700

    finished day 55

--- 55-higher-lower/server.py
+++ 55-higher-lower/server.py
@@ -1,36 +1,38 @@
-# 2. Create a new Flask application 
-from random import randrange
+# Flask application 
+from random import randint
 from flask import Flask
 app = Flask(__name__)
-# rand_int = randrange(0,10)
-RAND_INT = randrange(0,10)
+
+# variables
+rand_int = randint(0,9) # random number between 0 and 9
 
 
-# where the home route 
+
+# home route 
 @app.route('/')
 def home():
-    # displays an <h1> that says "Guess a number between 0 and 9" 
-    # and display https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif
     return '<h1>Guess a number between 0 and 9</h1>' \
         '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif" width="400">'
     
 
-# 3. Generate a random number between 0 and 9 or any range of numbers of your choice.
-# rand_int = randrange(0,10)
-# print(f"rand_int: {rand_int}")
 
+# detect the user's guess e.g "URL/3" or "URL/9" 
+@app.route('/<int:guess>')
+def guess(guess):
+    # checks that number against the generated random number.
+    if guess == rand_int:
+        # guessed the correct number
+        output = f'<div style="text-align: center;"><h1>you guessed right! {guess}</h1><p>way to go mang!</p><img src="https://media.giphy.com/media/WoUynUguj7wEP7HN0T/giphy.gif" width="350"></div>'
+        # rand_int = randint(0,10)
+    elif guess < rand_int:
+        # guess is too low 
+        output = f'<div style="text-align: center;"><h1>Your guess is too low</h1><p>you guessed {guess}</p><img src="https://media.giphy.com/media/fqst7AVqF6AVLlYklE/giphy.gif" width="350"><p>guess again {rand_int}</p></div>'
+    elif guess > rand_int:
+        # guess is too high 
+        output = f'<div style="text-align: center;"><h1>yu0r guess is to0 high</h1><p>yu0 gu3ssed {guess}</p><img src="https://media.giphy.com/media/26tPmgvOZmiDXBsyI/giphy.gif" width="350"><p>guess again {rand_int}</p></div>'
+    return output
 
-# 4. Create a route that can detect the number entered by the user e.g "URL/3" or "URL/9" 
-@app.route('/guess/<int:i>')
-def guess(i):
-    # return f'<p>rand_int: {rand_int}</p><p>i (from url): {i}</p>'
-    return f'<p>rand_int: {RAND_INT}</p><p>i (from url): {i}</p>'
-
-
-    # and checks that number against the generated random number. 
-    # If the number is too low, tell the user it's too low, https://media.giphy.com/media/fqst7AVqF6AVLlYklE/giphy.gif
-    # same with too high https://media.giphy.com/media/26tPmgvOZmiDXBsyI/giphy.gif
-    # or if they found the correct number. https://media.giphy.com/media/WoUynUguj7wEP7HN0T/giphy.gif
+    
 
     
     # try to make the <h1> text a different colour for each page. e.g. If the random number was 5:
