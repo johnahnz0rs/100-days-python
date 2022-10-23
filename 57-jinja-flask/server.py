45ccaf68dfbdf2c7d01f0e861580f5f230408b0d
johnahnz0rs <johnahn@z0rs.org>
2022-10-23 11:46:44 -0700

    con't day 57

--- 57-jinja-flask/server.py
+++ 57-jinja-flask/server.py
@@ -12,7 +12,7 @@ def home():
     return render_template("index.html", num=rando, copyright_year=copyright_year)
 
 
-@app.route('/guess/<name>')
+@app.route('/guess/<string:name>')
 def guess(name):
     guess_age = requests.get(url=f"https://api.agify.io/?name={name}")
     guess_gender = requests.get(url=f"https://api.genderize.io/?name={name}")
@@ -20,6 +20,13 @@ def guess(name):
     gender = guess_gender.json()["gender"]
 
     return render_template("guess.html", name=name, age=age, gender=gender)
+
+
+@app.route("/blog")
+def blog():
+    blog_response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
+    posts = blog_response.json()
+    return render_template("blog.html", posts=posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
